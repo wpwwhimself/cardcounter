7b9ed2395275f38970a3162f4df89f87b8681f8d
Wojciech Przybyła <contact@wpww.pl>
2026-07-09 10:29:14 +0200

    ✨ playing card asset

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,23 @@
+@props([
+    "id",
+])
+
+@php
+$rank = Str::after($id, '-');
+$color = Str::before($id, '-');
+$colors = [
+    "A" => "miecze",
+    "B" => "kielichy",
+    "C" => "berła",
+    "D" => "monety",
+    "E" => "taroty",
+];
+
+$card_path = "https://raw.githubusercontent.com/wpwwhimself/tarot-deck/master/kompletandos/pdftoimage/";
+$card_path .= "c$colors[$color]/c$colors[$color]-$rank.jpg";
+@endphp
+
+<div class="playing-card">
+    <img src="{{ asset('media/rewers.jpg') }}" alt="back" class="back">
+    <img src="{{ $card_path }}" alt="front" class="front">
+</div>
